360fbb7ea904d7388e6f15da51632867fd5e4c03
DESKTOP-V8M24KM\Jinelin Briones <jerome.briones@corptech.it>
2025-08-09 18:16:43 +0800

    End of Section 9

---
+++ client/src/app/features/shop/filters-dailog/filters-dailog.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject } from '@angular/core';
+import { ShopService } from '../../../core/services/shop.service';
+import { MatDivider } from '@angular/material/divider';
+import { MatListOption, MatSelectionList } from '@angular/material/list';
+import { MatButton } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-filters-dailog',
+  imports: [
+    MatDivider,
+    MatSelectionList,
+    MatListOption,
+    MatButton,
+    FormsModule
+  ],
+  templateUrl: './filters-dailog.component.html',
+  styleUrl: './filters-dailog.component.scss'
+})
+export class FiltersDailogComponent {
+  shopService = inject(ShopService);
+  private dialogRef = inject(MatDialogRef<FiltersDailogComponent>);
+  data = inject(MAT_DIALOG_DATA);
+
+  selectedBrands: string[] = this.data.selectedBrands;
+  selectedTypes: string[] = this.data.selectedTypes;
+
+  applyFilters() {
+    this.dialogRef.close({
+      selectedBrands: this.selectedBrands,
+      selectedTypes: this.selectedTypes
+    });
+  }
+}
